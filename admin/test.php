896af366fa50bc3d4ec65b1746d335953a662d5d
Daniel Mangowi <danielmangowi@gmail.com>
2016-08-08 01:23:48 +0200

    UPDATE and DELETE user

--- admin/test.php
+++ admin/test.php
@@ -6,14 +6,22 @@ if (!$session->is_logged_in()) { redirect_to("login.php"); }
 <?php include_layout_template('admin_header.php'); ?>
 
 <?php
-	$user = new User();
-	$user->username = "dansmith";
-	$user->password = "abcd12345";
-	$user->first_name = "Daniel";
-	$user->last_name = "Mangowi";
-	$user->create();
+  // $user = new User();
+  // $user->username = "johnsmith";
+  // $user->password = "abcd12345";
+  // $user->first_name = "John";
+  // $user->last_name = "Smith";
+  // $user->create();
+  
+  // $user = User::find_by_id(2);
+  // $user->password = "12345wxyz";
+  // $user->save();
+  
+  // $user = User::find_by_id(2);
+  // $user->delete();
+  // echo $user->first_name . " was deleted";
 ?>
 
 
 <?php include_layout_template('admin_footer.php'); ?>
-		
+    
