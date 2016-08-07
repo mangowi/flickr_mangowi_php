7a17691eeb95c03ae271178c759c1165b7b77594
Daniel Mangowi <danielmangowi@gmail.com>
2016-08-07 11:24:41 +0200

    Creating data base object

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-echo "Is this working?";
+require_once('../includes/database.php');
+
+if(isset($database)) { echo "true"; } else { echo "false"; }
+echo "<br />";
+
+echo $database->escape_value("It is working");
+
+//  $sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
+//  $sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Daniel', 'Mikonga')";
+// $result = $database->query($sql);
+
+$sql = "SELECT * FROM users WHERE id=1";
+$result = $database->query($sql);
+$found_user = $database->fetch_array($result);
+echo $found_user['username'];
+
 
 ?>
