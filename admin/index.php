abad7ef8899d723b2ea591d013e4d4bb8265dde3
Daniel Mangowi <danielmangowi@gmail.com>
2016-08-07 13:50:08 +0200

    Using path content for layout

--- admin/index.php
+++ admin/index.php
@@ -3,20 +3,11 @@ require_once("../../includes/initialize.php");
 
 if (!$session->is_logged_in()) { redirect_to("login.php"); }
 ?>
-<html>
-  <head>
-    <title>Mangowi Flickr Gallery</title>
-    <link href="../stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
-  </head>
-  <body>
-    <div id="header">
-      <h1>Photo Gallery</h1>
-    </div>
-    <div id="main">
-    <h2>Menu</h2>
+
+<?php include_layout_template('admin_header.php'); ?>
+
+  <h2>Menu</h2>
     
-    </div>
+
+<?php include_layout_template('admin_footer.php'); ?>
     
-    <div id="footer">Copyright <?php echo date("Y", time()); ?>, Daniel Mangowi</div>
-  </body>
-</html>
---
+++ layouts/admin_header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>Flickr Mangowi Gallery</title>
+    <link href="../stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="header">
+      <h1>Flickr Mangowi Gallery: Admin</h1>
+    </div>
+    <div id="main">
