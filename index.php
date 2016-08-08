28b45afb9980bf9b3115319e78a7efcb17644594
Daniel Mangowi <danielmangowi@gmail.com>
2016-08-08 10:50:13 +0200

    photograph class is done

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php require_once("../includes/initialize.php"); ?>
+<?php
+	// Find all photos
+	$photos = Photograph::find_all();
+?>
 
 <?php include_layout_template('header.php'); ?>
-<?php
-$user = User::find_by_id(1);
-echo $user->full_name();
 
-echo "<hr />";
+<?php foreach($photos as $photo): ?>
+  <div style="float: left; margin-left: 20px;">
+		<a href="photo.php?id=<?php echo $photo->id; ?>">
+			<img src="<?php echo $photo->image_path(); ?>" width="200" />
+		</a>
+    <p><?php echo $photo->caption; ?></p>
+  </div>
+<?php endforeach; ?>
 
-$users = User::find_all();
-foreach($users as $user) {
-  echo "User: ". $user->username ."<br />";
-  echo "Name: ". $user->full_name() ."<br /><br />";
-}
-
-?>
 <?php include_layout_template('footer.php'); ?>
